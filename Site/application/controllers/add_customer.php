4b2be62077935dbe5622ebdc7208e1180240d6f0
Arming Huang <arming@buildthedot.com>
2015-01-07 01:54:28 +0700

    .

--- Site/application/controllers/add_customer.php
+++ Site/application/controllers/add_customer.php
@@ -4,6 +4,9 @@ class Add_customer extends CI_Controller {
 
 	public function index()
 	{
+		// echo base_url();
+		// echo "asdf";
+		// exit;
 		$this->load->view('header');
 		$this->load->view('add_customer');
 	}
