a43f71bc527ec1a0f49d888de166b231b798030b
Arming Huang <arming@buildthedot.com>
2015-01-08 22:40:15 +0700

    add customer: add insert model

--- Site/application/models/customer_model.php
+++ Site/application/models/customer_model.php
@@ -22,6 +22,26 @@ class customer_model extends CI_Model
 		$work_code = ''
 	)
 	{
+		$data = array(
+			"cus_name" => $cus_name,
+			"cus_surname" => $cus_surname,
+			"home_address_1" => $home_address_1,
+			"home_address_2" => $home_address_2,
+			"home_city" => $home_city,
+			"home_country" => $home_country,
+			"home_code" => $home_code,
+			"work_address_1" => $work_address_1,
+			"work_address_2" => $work_address_2,
+			"work_city" => $work_city,
+			"work_country" => $work_country,
+			"work_code" => $work_code
+		);
+		if($this->db->insert("News", $data) == TRUE){
+			return TRUE;
+		}
+		else{
+			return FALSE;
+		}
 		
 	}
 }
